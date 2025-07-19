87c0fdbdb6ed8347fc07ec2de3cfa9234a14d39f
vishnuagarwal1545 <vishnuagarwal1545@gmail.com>
2025-07-19 14:37:53 +0000

    Deploying to gh-pages from @ vishnuagarwal1545/vishnuagarwal.github.io@61d481b57ec83f1827058fadc8a9d95610b1a1c4 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
+import PhotoSwipeLightbox from "";
+import PhotoSwipe from "";
 const photoswipe = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
